bc3437718624bfa198df1390be33ce7c3bd0b372
AJ Tatum <andrew.j.tatum@gmail.com>
2020-01-05 19:37:59 -0500

    Added ability to set response type

---
+++ SlackBotMessages/Enums/ResponseType.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SlackBotMessages.Enums
+{
+    public enum ResponseType
+    {
+        /// <summary>
+        /// Responses are visible to the entire channel
+        /// </summary>
+        in_channel,
+
+        /// <summary>
+        /// Responses are just visible to the user
+        /// </summary>
+        ephemeral
+    }
+}
